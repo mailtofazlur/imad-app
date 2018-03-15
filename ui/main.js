c3c21ab15e788ffe95192d64dbac7989d0c607bf
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-03-15 21:25:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,15 @@
 console.log('Loaded!');
+
+//change the text of main-text div
 var element = document.getElementById('main-text');
+
 element.innerHTML='New Content from Client Side Java Script';
+
+//Moving the image i.e. Animation
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = MarginLeft + 10;
+    marginLeft = MarginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
