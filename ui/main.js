293295e1f7ef0410917f8fdcf3646e5442da97ad
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-03-15 21:34:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML='New Content from Client Side Java Script';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 100;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
