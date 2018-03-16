d9b4ff8f8ee61cb81bcec45b491f1b1305f30295
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-03-16 23:38:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,8 +84,9 @@ app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
  });
+ 
  var counter = 0;
- app.get('/counter',function(req, res){
+ app.get('/:counter',function(req, res){
  counter = counter + 1;
  res.send(counter.toString());
  });
