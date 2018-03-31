ccc34602eff32443d92f6e98a609cd514ac1eb09
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-03-31 19:18:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app-get('/test-db',function (req, res){
+app.get('/test-db',function (req, res){
     //make a select request
     //return a response with the result
     pool.query('SELECT * FROM test', function (err,result){
