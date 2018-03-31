ec6dc4b20a7a804d8ed3687e1edacb96591813ef
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-03-31 18:12:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = required('pg').Pool;
 
+var config = {
+    user: 'fazlursmailbox',
+    database: 'fazlursmailbox',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -80,6 +88,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringyfy(result));
+        }
+    });
+});
  var counter = 0;
  app.get('/counter', function(req, res){
  counter = counter + 1;
