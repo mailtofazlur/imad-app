fa30c8f80522e2cb086cfcd77eb36a8924b74c81
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-03-31 18:15:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,27 +113,6 @@ app.get('/:articleName',function(req,res){
  
 
 
-/*app.get('/article-one',function(req,res){
- res.send(createTemplate(articleOne));
- // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-
-//    res.send('Article one is requested and will be served here');
-});
-
-app.get('/article-two',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
- 
-    
-    //res.send('Article two is requested and will be served here');
-});
-
-app.get('/article-three',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-    
-    
-    //res.send('Article three is requested and will be served here');
-}); */
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
