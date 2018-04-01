d38d92ea8eb60fd33528c34561cdd6bf1a3e9ac3
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-04-01 20:51:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ var htmlTemplate =`
                     ${heading}
                 </h3>
                 <div>
-                    ${date}
+                    ${date.toString()}
                 </div>
                 <div>
                     ${content}
