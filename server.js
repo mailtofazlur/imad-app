142db36a19d2cc2f29754b0d8bd010be6d664ad6
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-04-03 21:48:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/', function (req, res) {
 
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 1000,512, 'sha512');
-    return ["pbkdf2", "1000", salt, hashed.toString('hex')];
+    return ["pbkdf2", "1000", salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function(req, res){
