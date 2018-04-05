87bbc7e3069bdac7d2db7e0efc854d5964fddb34
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-04-05 23:53:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,29 @@
-//counter code
-var button = document.getElementById('counter');
-var counter = 0;
+//Submit username/password to login
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
 
-button.onclick = function(){
-
-    //Make the request to the counter endpoint
+    //create a request object
     var request = new XMLHttpRequest();
-
 
     //capture the response and store it in a variable
     request.onreadystatechange = function() {
         //Take some action
-        if(request.status == 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-            
+        if(request.status === 200){
+            alert('Logged in succesfully');
+        } else if(request.status === 400) {
+            alert('Username/password is incorrect');
+        } else if(request.status===500){
+            alert('Something went wrong on the server');
         }
     };
+    
     //Make the request
-    request.open('GET','http://fazlursmailbox.imad.hasura-app.io/counter',true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://fazlursmailbox.imad.hasura-app.io/login',true);
+    request.send(JSON.stringify({username:username,password:password}));
 };
 
 
@@ -41,6 +44,30 @@ button.onclick = function(){
 
 
 
+/*//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function(){
+
+    //Make the request to the counter endpoint
+    var request = new XMLHttpRequest();
+
+
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        //Take some action
+        if(request.status == 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+            
+        }
+    };
+    //Make the request
+    request.open('POST','http://fazlursmailbox.imad.hasura-app.io/counter',true);
+    request.send(null);
+}; */
 
 /*console.log('Loaded!');
 //change the text of main-text div
