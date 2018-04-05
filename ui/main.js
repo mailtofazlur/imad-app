233a53ceffca61ffe767033074c221cae7423c5c
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-04-06 00:32:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ submit.onclick = function(){
 
     //capture the response and store it in a variable
     request.onreadystatechange = function() {
+        if(request.readyState=== XMLHttpRequest.DONE)
         //Take some action
         if(request.status === 200){
             alert('Logged in succesfully');
