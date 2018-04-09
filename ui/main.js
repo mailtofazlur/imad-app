87436101a23d1f96902c81100ab326dc88ba4e52
Fazlur Rahman <fazlursmailbox@gmail.com>
2018-04-09 23:41:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,9 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE) {
         //Take some action
         if(request.status === 200) {
+            
             alert('Logged in succesfully');
-        } else if(request.status === 400) {
+        } else if(request.status === 403) {
             alert('Username/password is  incorrect');
         } else if(request.status === 500) {
             alert('Something went wrong on the server');
@@ -23,7 +24,7 @@ submit.onclick = function(){
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST','http://fazlursmailbox.imad.hasura-app.io/login',true);
+    request.open('POST','http://fazlursmailbox.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username:username,password:password}));
 };
